8ddd59313e87c5d31a8daa02d53e6d76dd479fe9
Brytch <77355787+xBrytch@users.noreply.github.com>
2022-04-06 17:19:43 -0300

    Update RoomSessionUserFigureUpdateEvent.ts

--- src/nitro/session/events/RoomSessionUserFigureUpdateEvent.ts
+++ src/nitro/session/events/RoomSessionUserFigureUpdateEvent.ts
@@ -5,6 +5,7 @@ export class RoomSessionUserFigureUpdateEvent extends RoomSessionEvent
 {
     public static USER_FIGURE: string = 'RSUBE_FIGURE';
 
+     private _roomIndex: number = 0;
     private _userId: number = 0;
     private _figure: string = '';
     private _gender: string = '';
@@ -12,7 +13,7 @@ export class RoomSessionUserFigureUpdateEvent extends RoomSessionEvent
     private _achievementScore: number;
     private _banner: string = '';
 
-    constructor(session: IRoomSession, userId: number, figure: string, gender: string, customInfo: string, achievementScore: number, banner: string)
+    constructor(session: IRoomSession, roomIndex: number, userId: number, figure: string, gender: string, customInfo: string, achievementScore: number, banner: string)
     {
         super(RoomSessionUserFigureUpdateEvent.USER_FIGURE, session);
 
@@ -22,6 +23,10 @@ export class RoomSessionUserFigureUpdateEvent extends RoomSessionEvent
         this._customInfo = customInfo;
         this._achievementScore = achievementScore;
         this._banner = banner;
+    }
+  public get roomIndex(): number
+    {
+        return this._roomIndex;
     }
 
     public get userId(): number
